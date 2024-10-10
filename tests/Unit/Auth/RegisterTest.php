6dcdc08208f0db40253162af2237663805e93f3f
CaweAlves <contato@cawe.dev>
2024-10-10 09:13:06 -0300

    gp-3: should not be possible to register a new user if the email is null

--- tests/Unit/Auth/RegisterTest.php
+++ tests/Unit/Auth/RegisterTest.php
@@ -26,3 +26,22 @@ it('should not be possible to register a new user if the email is invalid', func
 
     assertDatabaseCount('users', 0);
 });
+
+it('should not be possible to register a new user if the email is null', function () {
+    $name = $this->faker->name;
+
+    $payload = [
+        'name'                  => $name,
+        'email'                 => '',
+        'password'              => 'password',
+        'password_confirmation' => 'password',
+    ];
+
+    $response = postJson('/api/v1/register', $payload);
+
+    $response->assertStatus(422);
+    expect($response->json("message"))->toContain("The email field is required.");
+    ;
+
+    assertDatabaseCount('users', 0);
+});
