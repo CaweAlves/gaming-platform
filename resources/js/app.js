830e00b44f868a8b9ea1d99aa82c4ad0610ecd11
CaweAlves <contato@cawe.dev>
2024-10-16 13:53:59 -0300

    init frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import "./services/bootstrap.js";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/router.js";
+import store from "./stores/store.js";
+import Vue3Toastify from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+});
+
+app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        hmr: {
+            host: '127.0.0.1',
+        },
+        cors: true,
+        https: false,
+    },
+    build: {
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+        },
+    },
 });
